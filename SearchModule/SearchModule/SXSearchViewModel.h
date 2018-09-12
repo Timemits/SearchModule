5c2db40e4900f8fc15405ff6ca0b6fdb233bbdda
MrGCY <Mr_GCY@163.com>
2018-09-12 14:33:42 +0800

    修改错误

--- SearchModule/SearchModule/SXSearchViewModel.h
+++ SearchModule/SearchModule/SXSearchViewModel.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <ReactiveObjC/ReactiveObjC.h>
 @interface SXSearchViewModel : NSObject
 
 @property(nonatomic,strong)NSString *searchText;
